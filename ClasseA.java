832075ce7c8a5a7b8abc244bf8d0354881ee680d
Luiz Filipe Bartelega Penha <86612887+luizfilipe-bp@users.noreply.github.com>
2022-11-24 21:23:28 -0300

    Add files via upload

---
+++ ClasseA.java
@@ -0,0 +1,27 @@
+//Luiz Filipe Bartelega Penha
+//202111082
+public class ClasseA {
+    private int A1;
+    private float A2;
+
+    public int getA1(){
+        return A1;
+    }
+    public void setA1(int A1){
+        this.A1 = A1;
+    }    
+
+    public float getA2(){
+        return A2;
+    }
+    public void setA2(int A2){
+        this.A2 = A2;
+    }
+
+    public void MA1(){
+        System.out.println("MA1");
+    }
+    public void MA2(){
+        System.out.println("MA2");
+    }
+}
