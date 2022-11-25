0a8a48d25d19e114e429d0a20266c6ab4bd3140f
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2022-11-25 08:50:12 -0300

    Adicionado o arquivo ClasseD.java no projeto

---
+++ ClasseD.java
@@ -0,0 +1,27 @@
+//Luiz Filipe Bartelega Penha
+//202111082
+public class ClasseD {
+    private int D1;
+    private float D2;
+
+    public int getD1(){
+        return D1;
+    }
+    public void setD1(int D1){
+        this.D1 = D1;
+    }    
+
+    public float getD2(){
+        return D2;
+    }
+    public void setD2(int D2){
+        this.D2 = D2;
+    }
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
