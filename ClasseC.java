1867c7ef705d2f59d319536ceb9a61f65814802f
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2022-11-24 21:44:57 -0300

    Criação da Classe C

---
+++ ClasseC.java
@@ -0,0 +1,27 @@
+//Luiz Filipe Bartelega Penha
+//202111082
+public class ClasseC {
+    private int C1;
+    private float C2;
+
+    public int getC1(){
+        return C1;
+    }
+    public void setC1(int C1){
+        this.C1 = C1;
+    }    
+
+    public float getC2(){
+        return C2;
+    }
+    public void setC2(int C2){
+        this.C2 = C2;
+    }
+
+    public void MC1(){
+        System.out.println("MC1");
+    }
+    public void MC2(){
+        System.out.println("MC2");
+    }
+}
